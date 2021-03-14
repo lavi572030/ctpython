1fcb8c93fe545bb38bf6b60ba0faba5fedb945c6
lavi572030 <80192105+lavi572030@users.noreply.github.com>
2021-03-14 16:26:52 +0800

    Add files via upload

---
+++ HW01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 11:16:50 2021
+
+@author: user
+"""
+
+
+
+for i in range ( 5 , 0 , -1 ):
+    
+    for a in range ( 0 , i ):
+        print( '*' , end = '')
+    print()
+for i in range ( 2 , 6 ):
+    
+    for a in range ( 0 , i ):
+        print( '*' , end = '')
+    print()
